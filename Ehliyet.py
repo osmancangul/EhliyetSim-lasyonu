6e87c3d99af076167a1a0872f4998bcfc670c99c
osmancangul <75526353+osmancangul@users.noreply.github.com>
2022-07-29 14:33:45 +0300

    Add files via upload

---
+++ Ehliyet.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+ad = input("Adınızı Giriniz: ")
+soyad = input("Soyadınızı Giriniz: ")
+yas = int(input("Yaşınızı Giriniz: "))
+egitim_duzeyi = input("Eğitim Düzeyini Giriniz: ")
+
+# Ehliyet Alma Programı
+
+print("Merhaba {} {} ehliyet simülsasyonuna hoşgeldiniz.".format(ad,soyad))
+
+if yas >= 18:
+    print("Ehliyet Alabilirsiniz.")
+elif yas <= 18:
+    print("Ehliyet Alamazsınız.")
+elif egitim_duzeyi == "ilkokul" and "ortaokul" and "lise" 
+    print("Ehliyet Alabilirsiniz.")
+
+
+# In[ ]:
+
+
+
+
